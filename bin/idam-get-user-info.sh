54e67cfc20c74af2d947a4e0cb4c08a8267d419b
Aashish Aggarwal <aashish.aggarwal@hmcts.net>
2020-07-20 16:38:43 +0100

    Minor refactoring.

--- bin/idam-get-user-info.sh
+++ bin/idam-get-user-info.sh
@@ -2,13 +2,13 @@
 
 IMPORTER_USERNAME=${1:-am.docker.default@hmcts.net}
 IMPORTER_PASSWORD=${2:-Pa55word11}
-IDAM_OPEN_ID_URI=http://localhost:5000/o/token
+IDAM_OPEN_ID_URI=http://localhost:5000/o
 REDIRECT_URI="http://localhost:4096/oauth2redirect"
 CLIENT_ID="am_docker"
 CLIENT_SECRET="am_docker_secret"
 GRANT_TYPE="password"
 SCOPE_VARIABLES=openid%20profile%20roles%20authorities
 
-Authorization=$(curl -H "Content-Type: application/x-www-form-urlencoded" -XPOST "${IDAM_OPEN_ID_URI}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=${GRANT_TYPE}&scope=${SCOPE_VARIABLES}&username=${IMPORTER_USERNAME}&password=${IMPORTER_PASSWORD}" -d "" | jq -r .access_token)
+Authorization=$(curl -H "Content-Type: application/x-www-form-urlencoded" -XPOST "${IDAM_OPEN_ID_URI}/token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=${GRANT_TYPE}&scope=${SCOPE_VARIABLES}&username=${IMPORTER_USERNAME}&password=${IMPORTER_PASSWORD}" -d "" | jq -r .access_token)
 # Get the user Info using the OIDC token
-curl -H "Authorization: Bearer ${Authorization}"  -XGET 'http://localhost:5000/o/userinfo' | jq -r .uid
+curl -H "Authorization: Bearer ${Authorization}"  -XGET "${IDAM_OPEN_ID_URI}/userinfo" | jq -r .uid
