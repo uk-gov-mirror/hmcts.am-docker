bea7db4874e7c58a8c26b5c1d939e5608932ec98
Aashish Aggarwal <aashish.aggarwal@hmcts.net>
2020-11-10 18:52:34 +0000

    New Utils from iac docker

---
+++ bin/add-idam-clients.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -eu
+
+dir=$(dirname ${0})
+
+${dir}/utils/idam-create-service.sh "ccd_gateway" "ccd_gateway" "ccd_gateway_secret" "http://localhost:3451/oauth2redirect" "false" "openid profile roles authorities search-user"
+${dir}/utils/idam-create-service.sh "am_docker" "am_docker" "am_docker_secret" "http://localhost:4096/oauth2redirect" "false" "openid profile roles authorities search-user"
