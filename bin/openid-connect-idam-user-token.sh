56977b8b231a3112d51dc680c30efc90c03d85f8
Aashish Aggarwal <aashish.aggarwal@hmcts.net>
2020-12-18 15:47:09 +0000

    crd changes

--- bin/openid-connect-idam-user-token.sh
+++ bin/openid-connect-idam-user-token.sh
@@ -6,6 +6,6 @@ IDAM_OPEN_ID_URI=http://localhost:5000/o/token
 CLIENT_ID="am_docker"
 CLIENT_SECRET="am_docker_secret"
 GRANT_TYPE="password"
-SCOPE_VARIABLES=openid%20profile%20roles%20authorities
+SCOPE_VARIABLES=openid%20profile%20roles%20authorities%20create-user%20manage-user%20search-user
 
 curl ${CURL_OPTS} -H "Content-Type: application/x-www-form-urlencoded" -XPOST "${IDAM_OPEN_ID_URI}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=${GRANT_TYPE}&scope=${SCOPE_VARIABLES}&username=${IMPORTER_USERNAME}&password=${IMPORTER_PASSWORD}" -d "" | jq -r .access_token
--- bin/setup.sh
+++ bin/setup.sh
@@ -2,14 +2,17 @@
 
 # Setup Services
 echo "Setting up Services..."
-./add-idam-clients.sh
+#./add-idam-clients.sh
 
 # Setup Roles
-echo ""
-echo "Setting up Roles..."
-./utils/idam-add-role.sh "ccd-import"
-./utils/idam-add-role.sh "caseworker"
-./utils/idam-add-role.sh "caseworker-ia"
+#echo ""
+#echo "Setting up Roles..."
+#./utils/idam-add-role.sh "ccd-import"
+#./utils/idam-add-role.sh "caseworker"
+#./utils/idam-add-role.sh "caseworker-iac"
+#./utils/idam-add-role.sh "cwd-admin"
+#./utils/idam-add-role.sh "cwd-user"
+#./utils/idam-add-role.sh "caseworker-iac-bulkscan"
 
 ### Setup Users
 echo ""
@@ -19,9 +22,8 @@ echo "Setting up Users..."
 ./create-user.sh "ras.admin@hmcts.net" "testfirstname" "testsurname" "Pa55word11" "test" "[{ \"code\": \"caseworker\"}]"
 ./create-user.sh "orm.admin@hmcts.net" "testfirstname" "testsurname" "Pa55word11" "test" "[{ \"code\": \"caseworker\"}]"
 ./create-user.sh "TEST_AM_USER1_BEFTA@test.local" "testfirstname" "testsurname" "Pa55word11" "test" "[{ \"code\": \"caseworker\"}]"
-./create-user.sh "TEST_AM_USER2_BEFTA@test.local" "testfirstname" "testsurname" "Pa55word11" "test" "[{ \"code\": \"caseworker\"}]"
-./create-user.sh "TEST_AM_USER3_BEFTA@test.local" "testfirstname" "testsurname" "Pa55word11" "test" "[{ \"code\": \"caseworker\"}]"
-./create-user.sh "TEST_AM_USER4_BEFTA@test.local" "testfirstname" "testsurname" "Pa55word11" "test" "[{ \"code\": \"caseworker\"}, { \"code\": \"caseworker-ia\"}]"
-./create-user.sh "TEST_AM_USER5_BEFTA@test.local" "testfirstname" "testsurname" "Pa55word11" "test" "[{ \"code\": \"caseworker\"}, { \"code\": \"caseworker-ia\"}]"
-./create-user.sh "TEST_AM_USER6_BEFTA@test.local" "testfirstname" "testsurname" "Pa55word11" "test" "[{ \"code\": \"caseworker\"}, { \"code\": \"caseworker-ia\"}]"
-
+./create-user.sh "TEST_AM_USER2_BEFTA@test.local" "testfirstname" "testsurname" "Pa55word11" "test" "[{ \"code\": \"caseworker\"}, { \"code\": \"caseworker-iac\"}]"
+./create-user.sh "TEST_AM_USER3_BEFTA@test.local" "testfirstname" "testsurname" "Pa55word11" "test" "[{ \"code\": \"caseworker\"}, { \"code\": \"caseworker-iac\"}]"
+./create-user.sh "TEST_AM_USER6_BEFTA@test.local" "testfirstname" "testsurname" "Pa55word11" "test" "[{ \"code\": \"caseworker\"}, { \"code\": \"caseworker-iac\"}]"
+./create-user.sh "TEST_AM_USER4_BEFTA@test.local" "testfirstname" "testsurname" "Pa55word11" "test" "[{ \"code\": \"caseworker\"}, { \"code\": \"cwd-admin\"}]"
+./create-user.sh "TEST_AM_USER5_BEFTA@test.local" "testfirstname" "testsurname" "Pa55word11" "test" "[{ \"code\": \"caseworker\"}, { \"code\": \"cwd-user\"}, { \"code\": \"caseworker-iac-bulkscan\"}]"
